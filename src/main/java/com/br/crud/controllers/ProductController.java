d5d83d06e1ea1590481e89599e1439aef3f43b63
João Filipe <joaofilipeb045@gmail.com>
2023-07-01 00:05:54 -0300

    Create GET, POST and PUT

--- src/main/java/com/br/crud/controllers/ProductController.java
+++ src/main/java/com/br/crud/controllers/ProductController.java
@@ -1,15 +1,46 @@
 package com.br.crud.controllers;
 
+import com.br.crud.domain.product.Product;
+import com.br.crud.domain.product.ProductRepository;
+import com.br.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok("Ok!");
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data) {
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
     }
 }
